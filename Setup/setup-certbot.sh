bff6328255b2ac01cc350255407c384ab9481620
sierra-95 <michaelmachohi@gmail.com>
2024-02-21 16:42:59 +0300

    Nohup

--- Setup/setup-certbot.sh
+++ Setup/setup-certbot.sh
@@ -1,2 +1,5 @@
 sudo apt install certbot
+sudo apt-get install certbot python3-certbot-nginx
 sudo certbot --nginx -d web-01.holb20233m8xq2.tech
+
+#2: Redirect - Make all requests redirect to secure HTTPS access.
